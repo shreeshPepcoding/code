e1fe3f595f6b9d83689ba0430d3bf321e0167671
Shreesh Tripathi <shreeshtripathi@Shreeshs-MacBook-Pro.local>
2021-05-01 23:55:14 +0530

    updates

--- pp/TSP1/07TnS/tns.java
+++ pp/TSP1/07TnS/tns.java
@@ -130,11 +130,61 @@ public class tns {
         return sum;
     }
 
+    public static boolean isPrime(int num) {
+        for(int i = 2; i * i <= num; i++) {
+            if(num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void printPrime(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+            if(num == 0 || num == 1) continue;
+            if(isPrime(num) == true) {
+                System.out.println(num + " is prime");
+            } else {
+                System.out.println(num + " is not prime");
+            }
+        }
+    }
+
+    public static void sieve(int[] queries, int hi) {
+        boolean[] isPrime = new boolean[hi + 1];
+        // marks element element as true, just for conviniency
+        Arrays.fill(isPrime, true);
+
+        // pre calculate prime
+        for(int i = 2; i * i <= hi; i++) {
+            if(isPrime[i] == true) {
+                for(int j = i + i; j <= hi; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        // solve for every queries
+        for(int i = 0; i < queries.length; i++) {
+            int num = queries[i];
+            if(num == 0 || num == 1) continue;
+
+            if(isPrime[num] == true) {
+                System.out.println(num + " is prime");
+            } else {
+                System.out.println(num + " is not prime");
+            }
+        }
+    }
+
     public static void ques() {
+        int[] arr = arrayFiller(50, 1);
+        // display(arr);
+        // printPrime(arr);
+        sieve(arr, 9);
         // int[] arr = {1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0};
 
-        int res = polynomial(2, 5);
-        System.out.println(res);
+        // int res = polynomial(2, 5);
+        // System.out.println(res);
 
         // display(arr);
         // sort01(arr);
@@ -169,11 +219,86 @@ public class tns {
         return res;
     }
 
+    public static int[] mergeSort(int[] arr, int lo, int hi) {
+        // base case
+        if(lo == hi) {
+            int[] bres = new int[1];
+            bres[0] = arr[lo];
+            return bres;
+        }
+
+        int mid = lo + (hi - lo) / 2;
+        int[] arr1 = mergeSort(arr, lo, mid);
+        int[] arr2 = mergeSort(arr, mid + 1, hi);
+
+        int[] res = mergeTwoSortedArrays(arr1, arr2);
+
+        return res;
+    }
+
+    public static void partitionAnArray(int[] arr, int pivot) {
+        int i = 0;
+        int j = 0;
+
+        while(i < arr.length) {
+            if(arr[i] <= pivot) {
+                swap(arr, i, j);
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    public static int partitionIndex(int[] arr, int lo, int hi, int pivot) {
+        int i = lo;
+        int j = lo;
+
+        while(i <= hi) {
+            if(arr[i] <= pivot) {
+                swap(arr, i, j);
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+        return j - 1;
+    }
+
+    public static void quickSort(int[] arr, int lo, int hi) {
+        if(lo > hi) return;
+
+        int pivot = arr[hi];
+        int pi = partitionIndex(arr, lo, hi, pivot);    
+        quickSort(arr, lo, pi - 1);
+        quickSort(arr, pi + 1, hi);
+    }
+
     public static void sorting() {
-        return;
+        int[] arr = {90, 70, 20, 60, 50, 30, 80, 40, 10, 45};
+        
+        display(arr);
+        // partitionAnArray(arr, 45);
+        quickSort(arr, 0, arr.length - 1);
+        // int[] res = mergeSort(arr, 0, arr.length - 1);
+        // System.out.println(partitionIndex(arr, 0, arr.length - 1, 45));
+        display(arr);
+        // display(res);
     }
 
     // ~~~~~~~~~~~~~~~~~~~~~~~~Testing~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    public static int[] arrayFiller(int size, int digits) {
+        int power = (int)Math.pow(10, digits);
+
+        int[] arr = new int[size];
+        for(int i = 0; i < size; i++) {
+            arr[i] = (int)(Math.random() * power);
+        }
+        return arr;
+    }
+
     public static int[] mergeTwoSortedArrays2(int[] arr1, int[] arr2){
 
         // System.out.println(Arrays.toString(arr1));
@@ -213,13 +338,23 @@ public class tns {
   }
 
     public static void testing() {
-        int[] arr = {10, 20, 30, 40, 50};
-        int[] arr2 = {7, 9, 15, 21};
+        int[] arr = arrayFiller(10000, 3);
+        quickSort(arr, 0, arr.length - 1);
 
-        int[] res = mergeTwoSortedArrays2(arr, arr2);
-        System.out.println(Arrays.toString(res));
+        Arrays.sort(arr);
 
-        return;
+        long st = System.currentTimeMillis();
+        // int[] res = mergeSort(arr, 0, arr.length - 1);
+        // quickSort(arr, 0, arr.length - 1);
+        long end = System.currentTimeMillis();
+
+        System.out.println(end - st + " mili seconds");
+
+        // int[] arr = {10, 20, 30, 40, 50};
+        // int[] arr2 = {7, 9, 15, 21};
+
+        // int[] res = mergeTwoSortedArrays2(arr, arr2);
+        // System.out.println(Arrays.toString(res));
     }
 
     // ~~~~~~~~~~~~~~~~~~~~~~Main Sections~~~~~~~~~~~~~~~~~~~~~~~~~~~
