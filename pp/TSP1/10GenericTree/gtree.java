da4ae40048e184800d08dcf7f42d01853e17efbc
Shreesh Tripathi <shreeshtripathi@Shreeshs-MacBook-Pro.local>
2021-05-23 13:37:46 +0530

    updates

--- pp/TSP1/10GenericTree/gtree.java
+++ pp/TSP1/10GenericTree/gtree.java
@@ -88,16 +88,135 @@ public class gtree {
         return ht + 1;
     }
 
+    public static void traversals(Node node){
+        // node pre
+        System.out.println("Node Pre " + node.data);
+        for(Node child : node.children) {
+            // before call -> edge pre
+            System.out.println("Edge Pre " + node.data + "--" + child.data);
+            // call
+            traversals(child);
+            // after call -> edge post
+            System.out.println("Edge Post " + node.data + "--" + child.data);
+        }
+        // node post
+        System.out.println("Node Post " + node.data);
+    }
+
+    public static void levelOrder(Node node){
+        Queue<Node> qu = new ArrayDeque<>();
+
+        qu.add(node);
+
+        while(qu.size() > 0) {
+            // 1. get + remove
+            Node rem = qu.remove();
+            // 2. print
+            System.out.print(rem.data + " ");
+            // 3. add children
+            for(Node child : rem.children) {
+                qu.add(child);
+            }
+        }
+        System.out.println(".");
+    }
+
+    // level order using 2 queues
+    public static void levelOrderLinewise1(Node node){
+        Queue<Node> mainQ = new ArrayDeque<>();
+        Queue<Node> childQ = new ArrayDeque<>();
+        
+        mainQ.add(node);
+        int level = 1;
+        System.out.print("level - " + level + " : ");
+        while(mainQ.size() > 0) {
+            Node rem = mainQ.remove();
+            System.out.print(rem.data + " ");
+
+            for(Node child : rem.children) {
+                childQ.add(child);
+            }
+
+            if(mainQ.size() == 0) {
+                // hit enter
+                System.out.println();
+                level++;
+                // swap mainQ and childQ
+                Queue<Node> temp = mainQ;
+                mainQ = childQ;
+                childQ = temp;
+                if(mainQ.size() != 0)
+                    System.out.print("level - " + level + " : ");
+            }
+        }
+    }
+
+    // level order using delimiter
+    public static void levelOrderLinewise2(Node node){
+        // use linkedlist as a queue, because arrayDeque is not allow us to add null
+        Queue<Node> qu = new LinkedList<>();
+
+        qu.add(node);
+        qu.add(null);
+
+        while(qu.size() > 0) {
+            // 1. get + remove
+            Node rem = qu.remove();
+
+            if(rem == null) {
+                // 2. hit enter
+                System.out.println();
+                // 3. if size is more than 0 then again add null
+                if(qu.size() > 0)
+                    qu.add(null);
+
+            } else {
+                // 2. print
+                System.out.print(rem.data + " ");
+                // 3. add children
+                for(Node child : rem.children) {
+                    qu.add(child);
+                }
+            }
+        }
+    }
+
+    // level order line wise, using single que, count approach
+    public static void levelOrderLinewise3(Node node){
+        // use linkedlist as a queue, because arrayDeque is not allow us to add null
+        Queue<Node> qu = new LinkedList<>();
+        qu.add(node);
+
+        while(qu.size() > 0) {
+            int sz = qu.size();
+            
+            while(sz-- > 0) {
+                // 1. get + remove
+                Node rem = qu.remove();
+                // 2. print
+                System.out.print(rem.data + " ");
+                // 3. add children
+                for(Node child : rem.children) {
+                    qu.add(child);
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void fun() {
         Integer[] data = {10, 20, 50, null, 60, null, null, 30, 70, null, 80, 110, null, 120, null,
                                  null, 90, null, null, 40, 100, null, null, null};
         
         Node root = construct(data);
         display(root);
-        System.out.println("Size : " + size(root));
-        System.out.println("Max : " + max(root));
-        System.out.println("Min : " + min(root));
-
+        levelOrderLinewise3(root);
+        // levelOrderLinewise2(root);
+        // levelOrderLinewise1(root);
+        // levelOrder(root);
+        // System.out.println("Size : " + size(root));
+        // System.out.println("Max : " + max(root));
+        // System.out.println("Min : " + min(root));
     }
 
     public static void main(String[] args) {
