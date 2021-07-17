bd0dd20e187e028c3ad3ce5d60dc64b4f50cd323
Shreesh Tripathi <shreeshtripathi@Shreeshs-MacBook-Pro.local>
2021-07-17 14:13:29 +0530

    updates

--- pp/TSP/17_R&B/recursion.java
+++ pp/TSP/17_R&B/recursion.java
@@ -87,6 +87,166 @@ public class recursion {
         }
     }
 
+    // qpsf->queen placed so far, tq -> total queen, asf-> answer so far
+    public static void queensCombinations(int qpsf, int tq, int row, int col, String asf){
+        if(row == tq) {
+            if(qpsf == tq) {
+                System.out.println(asf);
+            }
+            return;
+        }
+        if(col + 1 < tq) {
+            // yes call
+            if(qpsf < tq)
+                queensCombinations(qpsf + 1, tq, row, col + 1, asf + "q");
+            // no call
+            queensCombinations(qpsf, tq, row, col + 1, asf + "-");
+        } else {
+            // yes call
+            if(qpsf < tq)
+                queensCombinations(qpsf + 1, tq, row + 1, 0, asf + "q\n");
+            // no call
+            queensCombinations(qpsf, tq, row + 1, 0, asf + "-\n");
+        }
+    }
+
+    public static void queensPermutations(int qpsf, int tq, int[][] chess){
+        if(qpsf == tq) {
+            // print result
+            for(int i = 0; i < chess.length; i++) {
+                for(int j = 0; j < chess[0].length; j++) {
+                    if(chess[i][j] != 0) {
+                        System.out.print("q" + chess[i][j] + "\t");
+                    } else {
+                        System.out.print("-\t");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 0; i < chess.length; i++) {
+            for(int j = 0; j < chess[0].length; j++) {
+                if(chess[i][j] == 0) {
+                    // place queen
+                    chess[i][j] = qpsf + 1;
+                    queensPermutations(qpsf + 1, tq, chess);
+                    // unplace queen
+                    chess[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public static void queensPermutations(int qpsf, int tq, int row, int col, String asf, boolean[] queens) {
+        if(row == tq) {
+            if(qpsf == tq) {
+                System.out.println(asf);
+                System.out.println();
+            }
+            return;
+        }
+        
+        // yes call
+        for(int q = 0; q < queens.length; q++) {
+            if(queens[q] == false) {
+                // place the queen
+                queens[q] = true;
+                if(col + 1 < tq) {
+                    queensPermutations(qpsf + 1, tq, row, col + 1, asf + "q" + (q + 1) + "\t", queens);
+                } else {
+                    queensPermutations(qpsf + 1, tq, row + 1, 0, asf + "q" + (q + 1) + "\n", queens);
+                }
+                // unplace queen
+                queens[q] = false;
+            }
+        }
+        // no call
+        if(col + 1 < tq) {
+            queensPermutations(qpsf, tq, row, col + 1, asf + "-\t", queens);
+        } else {
+            queensPermutations(qpsf, tq, row + 1, 0, asf + "-\n", queens);
+        }
+    }
+
+    public static void queensCombinations(int qpsf, int tq, boolean[][] chess, int i, int j){
+        if(qpsf == tq) {
+            for(int r = 0; r < chess.length; r++) {
+                for(int c = 0; c < chess[0].length; c++) {
+                    if(chess[r][c] == true) {
+                        System.out.print("q\t");
+                    } else {
+                        System.out.print("-\t");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+            return;
+        }
+
+        // travel in remaining columns in current row
+        for(int c = j + 1; c < chess[0].length; c++) {
+            int r = i;
+            // place
+            chess[r][c] = true;
+            queensCombinations(qpsf + 1, tq, chess, r, c);
+            // unplace
+            chess[r][c] = false;
+        }
+
+        // travel in remaining rows and columns
+        for(int r = i + 1; r < chess.length; r++) {
+            for(int c = 0; c < chess[0].length; c++) {
+                // place
+                chess[r][c] = true;
+                queensCombinations(qpsf + 1, tq, chess, r, c);
+                // unplace
+                chess[r][c] = false;
+            }
+        }
+    }
+
+    public static void queensCombinations2(int qpsf, int tq, boolean[][] chess, int i, int j){
+        if(qpsf == tq) {
+            for(int r = 0; r < chess.length; r++) {
+                for(int c = 0; c < chess[0].length; c++) {
+                    if(chess[r][c] == true) {
+                        System.out.print("q\t");
+                    } else {
+                        System.out.print("-\t");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+            return;
+        }
+
+        // travel in remaining columns in current row
+        for(int c = j + 1; c < chess[0].length; c++) {
+            int r = i;
+            // place
+            chess[r][c] = true;
+            queensCombinations(qpsf + 1, tq, chess, r, c);
+            // unplace
+            chess[r][c] = false;
+        }
+
+        // travel in remaining rows and columns
+        for(int r = i; r < chess.length; r++) {
+            for(int c = 0; c < chess[0].length; c++) {
+                // place
+                chess[r][c] = true;
+                queensCombinations(qpsf + 1, tq, chess, r, c);
+                // unplace
+                chess[r][c] = false;
+            }
+        }
+    }
+
     public static void fun() {
 
     }
