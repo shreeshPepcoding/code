991db04a879fc5760bf2ecfda00befdb198f9020
Shreesh Tripathi <shreeshtripathi@Shreeshs-MacBook-Pro.local>
2021-07-28 00:20:06 +0530

    updates

--- pp/TSP/17_R&B/recursion.java
+++ pp/TSP/17_R&B/recursion.java
@@ -1086,6 +1086,38 @@ public class recursion {
         }
 	}
 
+    // k-partition
+    public static int count = 1;
+    public static void solution(int i, int n, int k, int rssf, ArrayList<ArrayList<Integer>> ans) {
+        if(i > n) {
+            if(ans.size() == k) {
+                System.out.print(count + ". ");
+                for(int j = 0; j < ans.size(); j++) {
+                    ArrayList<Integer> list = ans.get(j);
+                    System.out.print(list + " ");
+                }
+                System.out.println();
+                count++;
+            }
+            return;
+        }
+        // n - 1, k work, add with previous options
+        for(int j = 0; j < ans.size(); j++) {
+            ArrayList<Integer> list = ans.get(j);
+            list.add(i);
+            solution(i + 1, n, k, rssf, ans);
+            list.remove(list.size() - 1);
+        }
+
+        // n - 1, k - 1, start from myself if size + 1 <= k
+        if(ans.size() + 1 <= k) {
+            ArrayList<Integer> mres = new ArrayList<>();
+            mres.add(i);
+            ans.add(mres);
+            solution(i + 1, n, k, rssf, ans);
+            ans.remove(ans.size() - 1);
+        }
+	}
 
     public static void fun() {
 
