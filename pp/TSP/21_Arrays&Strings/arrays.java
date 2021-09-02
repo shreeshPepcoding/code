af165da1ef81b8cf5d9770b57dbcf99b7f67c9d1
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2021-09-03 00:46:03 +0530

    update

--- pp/TSP/21_Arrays&Strings/arrays.java
+++ pp/TSP/21_Arrays&Strings/arrays.java
@@ -1,7 +1,5 @@
 import java.util.*;
 
-import jdk.vm.ci.meta.Value;
-
 public class arrays {
 
     // leetcode 925 https://leetcode.com/problems/long-pressed-name/
@@ -70,7 +68,7 @@ public class arrays {
     }
 
     // leetcode 169 https://leetcode.com/problems/majority-element/
-    public int majorityElement(int[] nums) {
+    public int majorityElement_(int[] nums) {
         int val = nums[0];
         int count = 1;
 
@@ -92,6 +90,163 @@ public class arrays {
         }
         // majority always exist, so val is result
         return val;
+    }
+
+    // leetcode 229 https://leetcode.com/problems/majority-element-ii/
+    private boolean checkFreqM2(int[] arr, int val) {
+        int count = 0;
+        for(int e : arr) {
+            if(e == val) count++;
+        }
+        return count > arr.length / 3;
+    }
+
+    public List<Integer> majorityElement(int[] arr) {
+        // find valid candidates fore majority
+        int val1 = arr[0];
+        int count1 = 1; // count1 -> count of val1 for current window to make triplets
+
+        int val2 = arr[0];
+        int count2 = 0; // count2 -> count of val2 for current window to make triplets
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] == val1) {
+                count1++;
+            } else if(arr[i] == val2) {
+                count2++;
+            } else {
+                if(count1 == 0) {
+                    val1 = arr[i];
+                    count1 = 1;
+                } else if(count2 == 0) {
+                    val2 = arr[i];
+                    count2 = 1;
+                } else {
+                    // triplet is found, reduce freq of val1 & val2 from count1 and count2
+                    count1--;
+                    count2--;
+                }
+            }
+        }
+
+        // check if it have freq more than n/3 using val1 and val2
+        List<Integer> list = new ArrayList<>();
+        boolean check1 = checkFreqM2(arr, val1);
+        if(check1 == true)  list.add(val1);
+
+        if(val1 == val2) return list;
+
+        boolean check2 = checkFreqM2(arr, val2);
+        if(check2 == true)  list.add(val2);
+        
+        return list;
+    }
+
+    // next greater element 3
+    private static int dipIndex(char[] arr) {
+        int indx = arr.length - 2;
+        while(indx >= 0 && (arr[indx] >= arr[indx + 1])) {
+            indx--;
+        }
+        return indx;
+    }
+
+    private static int ceilIndex(char[] arr, int indx) {
+        for(int i = arr.length - 1; i >= 0; i--) {
+            if(arr[i] > arr[indx]) {
+                return i;
+            }
+        }
+        return -1;
+    } 
+
+    private static void swap(char[] arr, int left, int right) {
+        char temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+
+    private static void reverse(char[] arr, int indx) {
+        int left = indx;
+        int right = arr.length - 1;
+        while(left < right) {
+            swap(arr, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    public static String nextGreaterElement_(String str) {
+        if(str.length() == 1) return "-1";
+        char[] arr = str.toCharArray();
+        int dipIndx = dipIndex(arr);
+        if(dipIndx == -1) {
+            return "-1";
+        }
+        int ceilIndx = ceilIndex(arr, dipIndx);
+        swap(arr, dipIndx, ceilIndx);
+        reverse(arr, dipIndx + 1);
+        return String.valueOf(arr);
+    }
+
+    // for leetcode, above problem
+    public int nextGreaterElement(int n) {
+        String str = n + "";
+        String res_ = nextGreaterElement_(str);
+        long res = Long.parseLong(res_);
+        if(res <= Integer.MAX_VALUE) {
+            return (int)res;
+        } else {
+            return -1;
+        }
+    }
+
+    // leetcode 628 https://leetcode.com/problems/maximum-product-of-three-numbers/
+    public int maximumProduct(int[] nums) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+
+        for(int val : nums) {
+            if(val > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = val;
+            } else if(val > max2) {
+                max3 = max2;
+                max2 = val;
+            } else if(val > max3) {
+                max3 = val;
+            }
+            if(val < min1) {
+                min2 = min1;
+                min1 = val;
+            } else if(val < min2) {
+                min2 = val;
+            }
+        }
+        return Math.max(max1 * min1 * min2, max1 * max2 * max3);
+    }
+
+    // leetcode 747 https://leetcode.com/problems/largest-number-at-least-twice-of-others/
+    public int dominantIndex(int[] nums) {
+        if(nums.length == 1) return 0;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int indx = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > max1) {
+                max2 = max1;
+                max1 = nums[i];
+                indx = i;
+            } else if(nums[i] > max2) {
+                max2 = nums[i];
+            }
+        }
+        return max1 >= 2 * max2 ? indx : -1;
     }
 
     public static void main(String[] args) {
