87f04645043f3b20a079b15895e3d3a962f8fbff
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2021-09-10 22:32:54 +0530

    update

--- pp/GTBIT_PAB/01_Arrays&String/arrays.java
+++ pp/GTBIT_PAB/01_Arrays&String/arrays.java
@@ -32,7 +32,120 @@ public class arrays {
 
     // leetcode 11. https://leetcode.com/problems/container-with-most-water/
     public int maxArea(int[] height) {
-           
+        int maxWater = 0;
+
+        int i = 0;
+        int j = height.length - 1;
+
+        while(i < j) {
+            // with i, j pair, water holding capacity
+            int l = j - i;
+            int h = Math.min(height[i], height[j]);
+            int water = l * h;
+
+            maxWater = Math.max(maxWater, water);
+
+            if(height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return maxWater;
+    }
+
+    // leetcode 977 https://leetcode.com/problems/squares-of-a-sorted-array/
+    public int[] sortedSquares(int[] nums) {
+        // o(n) time, O(n) space
+        int n = nums.length;
+        int[] res = new int[n];
+        int i = 0;
+        int j = n - 1;
+        
+        for(int k = n - 1; k >= 0; k--) {
+            int val1 = nums[i] * nums[i];
+            int val2 = nums[j] * nums[j];
+
+            if(val1 > val2) {
+                res[k] = val1;
+                i++;
+            } else {
+                res[k] = val2;
+                j--;
+            }
+        }
+        return res;
+    }
+
+    // leetcode 169. https://leetcode.com/problems/majority-element/ 
+    public int majorityElement1(int[] nums) {
+        int val = nums[0];
+        int count = 1;
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == val) {
+                count++;
+            } else {
+                if(count > 0) {
+                    // pairing for distinct elements
+                    count--;
+                } else {
+                    // reset
+                    val = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return val;
+    }
+
+    // leetcode 229. https://leetcode.com/problems/majority-element-ii/
+    private boolean isMajority(int[] arr, int val) {
+        int count = 0;
+        for(int ele : arr) {
+            if(val == ele)
+                count++;
+        }
+        return count > arr.length / 3;
+    }
+
+    public List<Integer> majorityElement(int[] nums) {
+        int val1 = nums[0];
+        int count1 = 1;
+        int val2 = nums[0];
+        int count2 = 0;
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == val1) {
+                count1++;
+            }  else if(nums[i] == val2) {
+                count2++;
+            } else {
+                // may be priplet, or a fresh start
+                if(count1 == 0) {
+                    val1 = nums[i];
+                    count1 = 1;
+                } else if(count2 == 0) {
+                    val2 = nums[i];
+                    count2 = 1;
+                } else {
+                    count1--;
+                    count2--;
+                }
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        // check if val1 and val2 present in majority or not
+        if(isMajority(nums, val1)) {
+            res.add(val1);
+        } 
+
+        if(val1 != val2 && isMajority(nums, val2)) {
+            res.add(val2);
+        }
+        return res;
     }
 
     public static void main(String[] args) {
