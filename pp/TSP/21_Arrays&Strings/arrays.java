9a69ca9df230f4c4096fa072ac7f74a61a280c41
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2021-09-20 23:59:47 +0530

    update

--- pp/TSP/21_Arrays&Strings/arrays.java
+++ pp/TSP/21_Arrays&Strings/arrays.java
@@ -1510,6 +1510,7 @@ public class arrays {
         }
         return count;
     }
+   
     public int minDominoRotations(int[] tops, int[] bottoms) {
         int val1 = tops[0];
         int val2 = bottoms[0];
@@ -1573,18 +1574,120 @@ public class arrays {
     }
 
     // leetcode 632. https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists/
+    private class pair implements Comparable<pair>{ 
+        int val; // value in list
+        int i; // row number
+        int j; // column number
+        public pair(int val, int i, int j) {
+            this.val = val;
+            this.i = i;
+            this.j = j;
+        }
+
+        public int compareTo(pair o) {
+            return this.val - o.val;
+        }
+    }
+
     public int[] smallestRange(List<List<Integer>> nums) {
-        
+        PriorityQueue<pair> pq = new PriorityQueue<>();
+        // push all values of column 0
+        int max = Integer.MIN_VALUE;
+        for(int r = 0; r < nums.size(); r++) {
+            // here column is fix and it is 0
+            int val = nums.get(r).get(0);
+            pq.add(new pair(val, r, 0));
+            max = Math.max(max, val);
+        }
+
+        int sp = 0;
+        int ep = 0;
+        int length = Integer.MAX_VALUE;
+
+        while(true) {
+            pair rem = pq.remove();
+            // add next value with same row
+            int cmin = rem.val;
+            int cmax = max;
+
+            if(cmax - cmin < length) {
+                length = cmax - cmin;
+                sp = cmin;
+                ep = cmax;
+            }
+
+            if(rem.j + 1 < nums.get(rem.i).size()) {
+                int val = nums.get(rem.i).get(rem.j + 1);
+                pq.add(new pair(val, rem.i, rem.j + 1));  
+                max = Math.max(max, val); 
+            } else {
+                break;
+            }
+        }
+        int[] res = {sp, ep};
+        return res;
     }
 
     // leetcode 1094. https://leetcode.com/problems/car-pooling/
     public boolean carPooling(int[][] trips, int capacity) {
-        
+        int len = 0;
+        // rows -> no. of trips
+        // column[0] -> number of passenger
+        // column[1] -> starting of trip
+        // column[2] -> ending of trip
+
+        int[] arr = new int[1001];
+
+        // travel on query and mark impact on array
+        for(int i = 0; i < trips.length; i++) {
+            int start = trips[i][1];
+            int end = trips[i][2];
+            int pass = trips[i][0];
+            arr[start] += pass;
+            arr[end] -= pass;
+
+            len = Math.max(len, end);
+        }
+
+        // make prefix sum to create impact of passenger's count
+        int prefixSum = 0;
+        for(int i = 0; i <= len; i++) {
+            prefixSum += arr[i];
+            arr[i] = prefixSum;
+
+            if(arr[i] > capacity) return false;
+        }
+        return true;
     }
 
     // leetcode 152. https://leetcode.com/problems/maximum-product-subarray/
     public int maxProduct(int[] nums) {
-        
+        int prod = 1;
+        int res = Integer.MIN_VALUE;
+
+        // left product
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                res = Math.max(res, nums[i]);
+                prod = 1; // for identity of product
+            } else {
+                prod *= nums[i];
+                res = Math.max(res, prod);
+            }
+        }
+
+        // right product
+        prod = 1;
+        for(int i = nums.length - 1; i >= 0; i--) {
+            if(nums[i] == 0) {
+                res = Math.max(res, nums[i]);
+                prod = 1; // for identity of product
+            } else {
+                prod *= nums[i];
+                res = Math.max(res, prod);
+            }
+        }
+        return res;
     }
 
     // leetcode 209. https://leetcode.com/problems/minimum-size-subarray-sum/
