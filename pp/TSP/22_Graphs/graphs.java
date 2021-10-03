00df8c774954cec2f0248194b1e8885859e8ad3c
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2021-10-03 14:15:50 +0530

    update

--- pp/TSP/22_Graphs/graphs.java
+++ pp/TSP/22_Graphs/graphs.java
@@ -676,6 +676,35 @@ public class graphs {
         return res;
     }
 
+    // leetcode 684. https://leetcode.com/problems/redundant-connection/
+    public int[] findRedundantConnection(int[][] edges) {
+        // number of vertices
+        int n = edges.length;
+
+        int[] parent = new int[n + 1];
+        int[] rank = new int[n + 1];
+        for(int i = 0; i <= n; i++) {
+            rank[i] = 1;
+            parent[i] = i;
+        }
+
+        for(int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            // find set leader of both u and v
+            int lu = find(parent, u);
+            int lv = find(parent, v);
+            if(lu == lv) {
+                // it means u-v edge is responsible for cycle
+                return edge;
+            }
+            union(lu, lv, rank, parent);
+        }
+        return new int[0];
+    }
+
+
     public static void main(String[] args) {
 
     }
