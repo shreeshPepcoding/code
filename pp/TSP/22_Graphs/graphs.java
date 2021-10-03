d9ba6271c4d2e37bb322eba2b1e466086f011eb3
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2021-10-03 22:52:02 +0530

    upadte

--- pp/TSP/22_Graphs/graphs.java
+++ pp/TSP/22_Graphs/graphs.java
@@ -828,13 +828,101 @@ public class graphs {
 
     // leetcode 990. https://leetcode.com/problems/satisfiability-of-equality-equations/
     public boolean equationsPossible(String[] equations) {
+        // process '==' and union then
+        int[] parent = new int[26];
+        int[] rank = new int[26];
+        for(int i = 0; i < 26; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+
+        for(int i = 0; i < equations.length; i++) {
+            if(equations[i].charAt(1) == '=') {
+                int x = equations[i].charAt(0) - 'a';
+                int y = equations[i].charAt(3) - 'a';
+
+                int lx = find(parent, x);
+                int ly = find(parent, y);
+
+                if(lx == ly) {
+                    continue;
+                }
+                union(lx, ly, rank, parent);
+            }
+        }
         
+        // check set leader for '!=' equality, if same then return false;
+        for(int i = 0; i < equations.length; i++) {
+            if(equations[i].charAt(1) == '!') {
+                int x = equations[i].charAt(0) - 'a';
+                int y = equations[i].charAt(3) - 'a';
+                int lx = find(parent, x);
+                int ly = find(parent, y);
+                if(lx == ly) return false;
+            }
+        }
+        return true;
     }
 
-    // lintcode 856. https://www.lintcode.com/problem/856/
-    public boolean isSentenceSimilarity(String[] words1, String[] words2, List<List<String>> pairs) {
-        
+    // leetcode 737, sentence similarity 2
+
+    private static HashMap<String, String> par; // parent
+    private static HashMap<String, Integer> ran; // rank
+
+    private static String find(String x) {
+        if(par.containsKey(x) == false) {
+            par.put(x, x);
+            ran.put(x, 1);
+        }
+
+        if(par.get(x).equals(x) == true) {
+            return x;
+        }
+        String temp = find(par.get(x));
+        par.put(x, temp);
+        return temp;
     }
+
+    private static void union(String u, String v) {
+        String lx = find(u);
+        String ly = find(v);
+
+        if(lx.equals(ly) == false) {
+            if(ran.get(lx) > ran.get(ly)) {
+                par.put(ly, lx);
+            } else if(ran.get(lx) < ran.get(ly)) {
+                par.put(lx, ly);
+            } else {
+                par.put(ly, lx);
+                ran.put(lx, ran.get(lx) + 1);
+            }
+        }
+    }
+
+    public static boolean areSentencesSimilarTwo(String[] Sentence1, String[] Sentence2, String[][] pairs) {
+        par = new HashMap<>();
+        ran = new HashMap<>();
+
+        if(Sentence1.length != Sentence2.length) return false;
+
+        for(String[] pair : pairs) {
+            union(pair[0], pair[1]);
+        }
+
+        for(int i = 0; i < Sentence1.length; i++) {
+            String w1 = Sentence1[i];
+            String w2 = Sentence2[i];
+
+            if(w1.equals(w2) == true || find(w1).equals(find(w2)) == true) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 
     public static void main(String[] args) {
 
