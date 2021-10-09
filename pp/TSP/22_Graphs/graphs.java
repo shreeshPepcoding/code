e1e7d872664c57c39e2e6ee3179ea6cbfb01b0b2
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2021-10-09 22:45:13 +0530

    update

--- pp/TSP/22_Graphs/graphs.java
+++ pp/TSP/22_Graphs/graphs.java
@@ -922,6 +922,114 @@ public class graphs {
         return true;
     }
 
+    // ====================CYCLE=======================
+    // cycle in undirected graph, https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+    public boolean isCycle1(int V, ArrayList<ArrayList<Integer>> adj) {
+        
+    }
+
+    
+    // cycle in directed graph, https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
+    public boolean isCyclic2(int V, ArrayList<ArrayList<Integer>> adj) {
+        
+    }
+
+    // leetcode 207, https://leetcode.com/problems/course-schedule/
+    public boolean canFinish(int n, int[][] edges) {
+        // prepare graph
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for(int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+        }
+        
+        // make an indegree array from graph
+        int[] indegree = new int[n];
+        for(int i = 0; i < n; i++) {
+            for(int nbr : graph.get(i)) {
+                indegree[nbr]++;
+            }
+        }
+
+        // add element in queue which have 0 indegree
+        Queue<Integer> qu = new LinkedList<>();
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) {
+                qu.add(i);
+            }
+        }
+
+        int count = 0;
+        while(qu.size() > 0) {
+            // 1. get + remove
+            int rem = qu.remove();
+            // 2. print*
+            count++;
+            // 3. decrease indegree of nbr and if indegree become 0 then add in queue
+            for(int nbr : graph.get(rem)) {
+                indegree[nbr]--;
+                if(indegree[nbr] == 0) {
+                    qu.add(nbr);
+                }
+            }
+        }
+        return count == n;
+    }
+
+    // leetcode 210, https://leetcode.com/problems/course-schedule-ii/
+    public int[] findOrder(int n, int[][] edges) {
+        // prepare graph
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for(int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+        }
+        
+        // make an indegree array from graph
+        int[] indegree = new int[n];
+        for(int i = 0; i < n; i++) {
+            for(int nbr : graph.get(i)) {
+                indegree[nbr]++;
+            }
+        }
+
+        // add element in queue which have 0 indegree
+        Queue<Integer> qu = new LinkedList<>();
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) {
+                qu.add(i);
+            }
+        }
+
+        int count = 0;
+        int[] res = new int[n];
+        int indx = n - 1;
+        while(qu.size() > 0) {
+            // 1. get + remove
+            int rem = qu.remove();
+            // 2. print*
+            res[indx] = rem;
+            indx--;
+            count++;
+            // 3. decrease indegree of nbr and if indegree become 0 then add in queue
+            for(int nbr : graph.get(rem)) {
+                indegree[nbr]--;
+                if(indegree[nbr] == 0) {
+                    qu.add(nbr);
+                }
+            }
+        }
+        if(count != n) {
+            res = new int[0];
+            return res;
+        }
+        return res;
+    }
+
 
 
     public static void main(String[] args) {
