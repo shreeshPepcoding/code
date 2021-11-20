4f4d9f5844de4836abc89b141563177999b4b40c
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2021-11-20 15:59:33 +0530

    update

--- pp/Merged/01Searching&Sorting/SnS.java
+++ pp/Merged/01Searching&Sorting/SnS.java
@@ -721,6 +721,36 @@ public class SnS {
         return cnt; 
     }
 
+    // leetcode 540. https://leetcode.com/problems/single-element-in-a-sorted-array/
+    public int singleNonDuplicate(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        int indx = -1;
+        
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if((mid == 0 || mid == nums.length - 1) || 
+                (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1])) {
+                indx = mid;
+                break;
+            } else if(nums[mid] == nums[mid + 1]){
+                if((hi - mid + 1) % 2 == 0) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid;
+                }
+            } else {
+                if((mid - lo + 1) % 2 == 0) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid;
+                }
+            }
+        }
+        
+        return nums[indx];
+    }
+
     public static void main(String[] args) {
         
     }
