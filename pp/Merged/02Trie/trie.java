538729f7e8d6dc7fffedf6c000969450407c09f2
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2021-11-27 23:17:51 +0530

    update

--- pp/Merged/02Trie/trie.java
+++ pp/Merged/02Trie/trie.java
@@ -127,17 +127,17 @@ public class trie {
 
     // leetcode 212, https://leetcode.com/problems/word-search-ii/
 
-    private class Node {
-        Node[] children;
-        boolean isEnd;
-        int freq;
+    // private class Node {
+    //     Node[] children;
+    //     boolean isEnd;
+    //     int freq;
 
-        Node() {
-            this.children = new Node[26];
-            this.isEnd = false;
-            this.freq = 0;
-        }
-    }
+    //     Node() {
+    //         this.children = new Node[26];
+    //         this.isEnd = false;
+    //         this.freq = 0;
+    //     }
+    // }
 
     private void insert(String word, Node root) {
         for(int i = 0; i < word.length(); i++) {
@@ -146,6 +146,7 @@ public class trie {
                 root.children[ch - 'a'] = new Node();
             }
             root = root.children[ch - 'a'];
+            root.freq++;
         }
         root.isEnd = true;
     }
@@ -153,29 +154,36 @@ public class trie {
     private int[] xdir = {-1, 0, 1, 0};
     private int[] ydir = {0, -1, 0, 1};
 
-    private void travelAndAdd(char[][] board, int i, int j, boolean[][] vis,
+    private int travelAndAdd(char[][] board, int i, int j, boolean[][] vis,
          Node root, List<String> res, StringBuilder str) {
         char ch = board[i][j];
         if(root.children[ch - 'a'] == null)
-            return;
+            return 0;
         
+        root = root.children[ch - 'a'];
+        if(root.freq == 0) {
+            return 0;
+        }
+        int count = 0;
         str.append(ch);
         vis[i][j] = true;
-        root = root.children[ch - 'a'];
         if(root.isEnd == true) {
             res.add(str.toString());
             root.isEnd = false;
+            count = 1;
         }
         for(int d = 0; d < 4; d++) {
             int r = i + xdir[d];
             int c = j + ydir[d];
 
             if(r >= 0 && r < board.length && c >= 0 && c < board[0].length && vis[r][c] == false) {
-                travelAndAdd(board, r, c, vis, root, res, str);
+                count += travelAndAdd(board, r, c, vis, root, res, str);
             }
         }
         str.deleteCharAt(str.length() - 1);
         vis[i][j] = false;
+        root.freq -= count;
+        return count;
     }
 
     public List<String> findWords(char[][] board, String[] words) {
@@ -196,6 +204,73 @@ public class trie {
         return res;
     }
 
+    // leetcode 648, https://leetcode.com/problems/replace-words/
+    public String replaceWords(List<String> dictionary, String sentence) {
+        
+    }
+
+    // leetcode 472. https://leetcode.com/problems/concatenated-words/
+    private class Node {
+        Node[] children;
+        String s;
+        boolean isAdded;
+        Node() {
+            this.children = new Node[26];
+        }
+    }
+
+    private void insert(Node node, String word) {
+        if(word.length() == 0) return;
+        for(int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if(node.children[ch - 'a'] == null) {
+                node.children[ch - 'a'] = new Node();
+            }
+            node = node.children[ch - 'a'];
+        }
+        node.s = word;
+    }
+
+    private ArrayList<String> res;
+
+    private void matchCharacter(Node p1, Node p2, Node root) {
+        if(p1.s != null && p2.s != null) {
+            // p1 id ptr from dfsOnTRIE
+            if(p1.isAdded == false) 
+                res.add(p1.s);
+            p1.isAdded = true;
+        }
+        else if(p2.s != null) {
+            matchCharacter(p1, root, root);
+        }
+        for(int i = 0; i < 26; i++) {
+            if(p1.children[i] != null && p2.children[i] != null) {
+                matchCharacter(p1.children[i], p2.children[i], root);
+            }
+        }
+    }
+
+    private void dfsOnTRIE(Node ptr, Node root) {
+        if(ptr.s != null) {
+            matchCharacter(ptr, root, root);
+        }
+        for(Node child : ptr.children) {
+            if(child != null) {
+                dfsOnTRIE(child, root);
+            }
+        }
+    }
+
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        Node root = new Node();
+        for(String word : words) {
+            insert(root, word);
+        }
+        res = new ArrayList<>();
+        dfsOnTRIE(root, root);
+        return res;
+    }
     
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();   
