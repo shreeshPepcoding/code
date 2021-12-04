57f9222c6c121a34eb1b2daf09e1b8d3e409a64c
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2021-12-04 13:48:32 +0530

    update

--- pp/TSP/25_Stacks/stacks.java
+++ pp/TSP/25_Stacks/stacks.java
@@ -97,6 +97,129 @@ public class stacks {
         
     }
 
+    // largest area histogram, leetcode 84
+
+    private int[] nsrIndex(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        // push index in stack
+        int[] res = new int[arr.length];
+        for(int i = 0; i < arr.length; i++) {
+            while(st.size() > 0 && arr[st.peek()] > arr[i]) {
+                res[st.pop()] = i;
+            }
+            st.push(i);
+        }
+        while(st.size() > 0) {
+            res[st.pop()] = arr.length;
+        }
+        return res;
+    }
+
+    private int[] nslIndex(int[] arr) {
+        Stack<Integer> st = new Stack<>();
+        // push index in stack
+        int[] res = new int[arr.length];
+        for(int i = arr.length - 1; i >= 0; i--) {
+            while(st.size() > 0 && arr[st.peek()] > arr[i]) {
+                res[st.pop()] = i;
+            }
+            st.push(i);
+        }
+        while(st.size() > 0) {
+            res[st.pop()] = -1;
+        }
+        return res;
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        int[] nsr = nsrIndex(heights);
+        int[] nsl = nslIndex(heights);
+        int ans = 0;
+        for(int i = 0; i < heights.length; i++) {
+            int area = (nsr[i] - nsl[i] - 1) * heights[i];
+            ans = Math.max(ans, area);
+        }
+        return ans;
+    }
+
+    // maximal rectangle, leetcode 85
+    public int maximalRectangle(char[][] matrix) {
+        int[] ht = new int[matrix[0].length];
+        int res = 0;
+        for(int i = 0; i < ht.length; i++) {
+            
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == '0') {
+                    ht[j] = 0;
+                } else {
+                    ht[j] += 1;
+                }
+            }
+            res = Math.max(res, largestRectangleArea(ht));
+        }
+        return res;
+    }
+
+    // leetcode 921.
+    public int minAddToMakeValid(String s) {
+        Stack<Character> st = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(' || st.size() == 0) {
+                st.push(ch);
+            } else if(st.peek() == '('){
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+        return st.size();
+    }
+
+    // leetcode 1093 https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/
+    public int minSwaps(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '[' || st.size() == 0) {
+                st.push(ch);
+            } else if(st.peek() == '['){
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+        int pair = st.size() / 2;
+        return (int)Math.ceil(pair / 2.0);
+    }
+    
+    // count reversal https://www.geeksforgeeks.org/minimum-number-of-bracket-reversals-needed-to-make-an-expression-balanced/
+    private int countRev (String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '{' || st.size() == 0) {
+                st.push(ch);
+            } else if(st.peek() == '}'){
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+        int op = 0;
+        int cl = 0;
+        while(st.size() > 0) {
+            char ch = st.pop();
+            if(ch == '{') {
+                op++;
+            } else {
+                cl++;
+            }
+        }
+
+        return (int)(Math.ceil(op / 2.0) + Math.ceil(cl / 2.0));
+    }
     public static void main(String[] args) {
         
     }
