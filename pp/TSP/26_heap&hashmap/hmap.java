0da75851fd7af372c37e1712cdd9b1fd952220d2
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2021-12-26 14:56:37 +0530

    update

---
+++ pp/TSP/26_heap&hashmap/hmap.java
@@ -0,0 +1,132 @@
+import java.util.*;
+
+public class hmap {
+
+    private static Scanner scn = new Scanner(System.in);
+
+    // number of employee under every manager, portal
+    private static int printCountOfEmployee(HashMap<String, HashSet<String>> tree, String root) {
+        if(tree.get(root) == null) {
+            System.out.println(root + " 0");
+            return 1;
+        }
+        int count = 0;
+        for(String child : tree.get(root)) {
+            count += printCountOfEmployee(tree, child);
+        }
+        System.out.println(root + " " + count);
+        return count + 1;
+    }
+
+    public static void numberOfEmployee() {
+        int n = scn.nextInt();
+        HashMap<String, HashSet<String>> tree = new HashMap<>();
+        String root = "";
+        for(int i = 0; i < n; i++) {
+            String emp = scn.next();
+            String man = scn.next();
+            if(emp.equals(man) == true) {
+                root = emp;
+                continue;
+            }
+            if(tree.containsKey(man) == true) {
+                tree.get(man).add(emp);
+            } else {
+                HashSet<String> list = new HashSet<>();
+                list.add(emp);
+                tree.put(man, list);
+            }
+        }
+        printCountOfEmployee(tree, root);
+    }
+
+    // find itinerary from tickets
+    public static void printIntinerary(HashMap<String, String> connections) {
+        HashMap<String, Boolean> begin = new HashMap<>();
+        for(String start : connections.keySet()) {
+            String end = connections.get(start);
+            begin.put(end, false); // update or insert
+            if(begin.containsKey(start) == false) {
+                begin.put(start, true);
+            }
+        }
+        String src = "";
+        for(String key : begin.keySet()) {
+            if(begin.get(key) == true) {
+                src = key;
+                break;
+            }
+        }
+
+        System.out.print(src);
+        while(connections.containsKey(src)) {
+            src = connections.get(src);
+            System.out.print(" -> " + src);
+        }
+        System.out.println(".");
+    }
+
+    // count distinct element, portal
+    public static ArrayList<Integer> solution(int[] arr, int k) {
+        HashMap<Integer, Integer> fmap = new HashMap<>();
+        // add element from 0 to k-1
+        for(int i = 0; i < k - 1; i++) {
+            int ofq = fmap.getOrDefault(arr[i], 0);
+            fmap.put(arr[i], ofq + 1);
+        }
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = k - 1; i < arr.length; i++) {
+            // add current element
+            int ofq = fmap.getOrDefault(arr[i], 0);
+            fmap.put(arr[i], ofq + 1);
+            // add size of fmap in result
+            res.add(fmap.size());
+            // reduce freq of first element of window, if freq is 0 then remove it from window
+            int j = i - k + 1;
+            ofq = fmap.get(arr[j]);
+            if(ofq == 1) {
+                fmap.remove(arr[j]);
+            } else {
+                fmap.put(arr[j], ofq - 1);
+            }
+        }
+        return res;
+	}
+
+    // Check If An Array Can Be Divided Into Pairs Whose Sum Is Divisible By K, portal
+    public static void solution(int[] arr, int k){
+		HashMap<Integer, Integer> map = new HashMap<>();
+        // prepare freq map of remainders
+        for(int val : arr) {
+            int rem = val % k;
+            int ofq = map.getOrDefault(rem, 0);
+
+            map.put(rem, ofq + 1);
+        }
+
+        for(int rem : map.keySet()) {
+            if(rem == 0) {
+                if(map.get(rem) % 2 == 1) {
+                    System.out.println(false);
+                    return;
+                }
+            } else if(rem * 2 == k) {
+                if(map.get(rem) % 2 == 1) {
+                    System.out.println(false);
+                    return;
+                }
+            } else {
+                int rem2 = k - rem;
+                if(map.get(rem2) != map.get(rem)) {
+                    System.out.println(false);
+                    return;
+                }
+            }
+        }
+        System.out.println(true);
+	}
+
+    public static void main(String[] args) {
+
+    }
+}
