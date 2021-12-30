ed474de349011eb4c66f7a64b05bce13fd8fc93e
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2021-12-31 00:22:50 +0530

    update

--- pp/TSP/26_heap&hashmap/hmap.java
+++ pp/TSP/26_heap&hashmap/hmap.java
@@ -125,6 +125,110 @@ public class hmap {
         }
         System.out.println(true);
 	}
+    
+    // largest subarray with 0 sum
+    public static int solution(int[] arr) {
+		int psum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int len = 0;
+        map.put(0, -1);
+        for(int i = 0; i < arr.length; i++) {
+            int val = arr[i];
+            psum += val;
+            if(map.containsKey(psum) == false) {
+                map.put(psum, i);
+            } else {
+                len = Math.max(len, i - map.get(psum));
+            }
+        }
+		return len;
+	}
+
+    // Number of Subarray having sum is equal to 0
+    public static int countSubArray1(int[] arr) {
+		int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int psum = 0;
+        for(int i = 0; i < arr.length; i++) {
+            int val = arr[i];
+            psum += val;
+            if(map.containsKey(psum) == false) {
+                map.put(psum, 1);
+            } else {
+                int ofq = map.get(psum);
+                count += ofq;
+                map.put(psum, ofq + 1);
+            }
+        }
+		return count;
+	}
+
+    public static int longest01SubArray(int[] arr) {
+        // step 1 : change 0 to -1
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == 0) {
+                arr[i] = -1;
+            }
+        }
+        // step 2 : solve for longest subarray having sum equal to 0
+        return solution(arr);
+    }
+
+    public static int count01SubArray(int[] arr) {
+        // step 1 : change 0 to -1
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == 0) {
+                arr[i] = -1;
+            }
+        }
+        // step 2 : solve for count subarray having sum equal to 0
+        return countSubArray1(arr);
+    }
+
+    // longest length of subarray having sum equal to K
+    public static int maxLenSubarray(int[] nums, int k) {
+        int psum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int len = 0;
+        for(int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            psum += val;
+            if(map.containsKey(psum - k) == true) {
+                len = Math.max(len, i - map.get(psum - k));
+            }
+
+            if(map.containsKey(psum) == false) {
+                map.put(psum, i);
+            }
+        }
+        return len;
+    }
+
+    // count subarray having sum is equal to K
+    public static int countSumK(int[] nums, int k){
+		int psum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            psum += val;
+            if(map.containsKey(psum - k) == true) {
+                count += map.get(psum - k);
+            }
+
+            if(map.containsKey(psum) == false) {
+                map.put(psum, 1);
+            } else {
+                map.put(psum, map.get(psum) + 1);
+            }
+        }
+        return count;
+	}
+	
+
 
     public static void main(String[] args) {
 
