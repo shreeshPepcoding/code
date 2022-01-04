907eaf3ba5e827fbbb2f058e2660efb3c459ef02
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2022-01-05 00:25:25 +0530

    update

--- pp/TSP/26_heap&hashmap/hmap.java
+++ pp/TSP/26_heap&hashmap/hmap.java
@@ -354,6 +354,149 @@ public class hmap {
         }
 	}
 
+    // smallest substring of string containing all characters of another string
+    public static String smallestSubStringAllChar(String s1, String s2){
+        // frequency map for string 2
+        HashMap<Character, Integer> fmap = new HashMap<>();
+        for(int i = 0; i < s2.length(); i++) {
+            char ch = s2.charAt(i);
+            fmap.put(ch, fmap.getOrDefault(ch, 0) + 1);
+        }
+        
+        // move toward solution
+        int acq = -1; // acquire
+        int rel = -1; // release
+        int count = 0;
+        int requirement = s2.length();
+        HashMap<Character, Integer> map = new HashMap<>();
+        String ans = "";
+        while(true) {
+            boolean acFlag = false; // acquire flag
+            boolean reFlag = false; // release flag
+            // acquire
+            while(acq < s1.length() - 1 && count < requirement) {
+                acq++;
+                char ch = s1.charAt(acq);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+                // conditional increment in count
+                if(map.get(ch) <= fmap.getOrDefault(ch, 0)) {
+                    count++;
+                }
+                acFlag = true;
+            }
+        
+            // release
+            while(count == requirement) {
+                // hold ans, and if smallest then update the result
+                String tempAns = s1.substring(rel + 1, acq + 1);
+                if(ans.length() == 0 || tempAns.length() < ans.length()) {
+                    ans = tempAns;
+                }
+                // get the character and remove from map(either dec. freq. or remove key from map)
+                rel++;
+                char ch = s1.charAt(rel);
+                map.put(ch, map.get(ch) - 1);
+                if(map.get(ch) == 0) map.remove(ch);
+
+                // decrement in count if release is invalid
+                if(map.getOrDefault(ch, 0) < fmap.getOrDefault(ch, 0)) {
+                    count--;
+                }
+                reFlag = true;    
+            }
+            
+            // conditional break from loop
+            if(acFlag == false && reFlag == false) {
+                break;
+            }
+        }
+        return ans;
+	}
+
+    // smallest substring of a string containing all unique character of itself
+    private static int smallestSubStringAllUniqueChar(String str) {
+        HashSet<Character> set = new HashSet<>();
+        for(int i = 0; i < str.length(); i++) {
+            set.add(str.charAt(i));
+        }
+
+        int acq = -1;
+        int rel = -1;
+        int ansLen = Integer.MAX_VALUE;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while(true) {
+            boolean acflag = false;
+            boolean reflag = false;
+            // acquire
+            while(acq < str.length() - 1 && map.size() < set.size()) {
+                acq++;
+                char ch = str.charAt(acq);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+                acflag = true;
+            }
+            // release
+            while(map.size() == set.size()) {
+                int tempLen = acq - rel;
+                if(tempLen < ansLen) {
+                    ansLen = tempLen;
+                }
+
+                rel++;
+                char ch = str.charAt(rel);
+                map.put(ch, map.get(ch) - 1);
+                if(map.get(ch) == 0) map.remove(ch);
+
+                reflag = true;
+            }
+
+            if(acflag == false && reflag == false) break;
+        }
+        return ansLen;
+    }
+
+    // longest substring with non repeating character
+    public static int longestSubStringNonRepeating(String str) {
+        int ansLen = 0;
+        int acq = -1;
+        int rel = -1;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while(true) {
+            boolean acflag = false;
+            boolean reflag = false;
+
+            // acquire
+            while(acq < str.length() - 1) {
+                acflag = true;
+                acq++;
+                char ch = str.charAt(acq);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+                if(map.get(ch) == 2) {
+                    // stop acq.
+                    break;
+                } else {
+                    int tempLen = acq - rel;
+                    if(tempLen > ansLen) ansLen = tempLen;
+                }
+            }
+            // release
+            while(rel < acq) {
+                reflag = true;
+                rel++;
+                char ch = str.charAt(rel);
+                map.put(ch, map.get(ch) - 1);
+                if(map.get(ch) == 1) {
+                    // after reducing, if freq is 1, that means this ch charcacter is responsible for repition
+                    break;
+                }
+            }
+
+            if(acflag == false && reflag == false) break;
+        }
+        return ansLen;
+	}
+
     public static void main(String[] args) {
 
     }
