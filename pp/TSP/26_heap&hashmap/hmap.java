ee523045dd0a2a86c5bcd93aefd53ece6d47e46b
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2022-01-08 01:11:19 +0530

    update

--- pp/TSP/26_heap&hashmap/hmap.java
+++ pp/TSP/26_heap&hashmap/hmap.java
@@ -67,7 +67,7 @@ public class hmap {
     }
 
     // count distinct element, portal
-    public static ArrayList<Integer> solution(int[] arr, int k) {
+    public static ArrayList<Integer> solution1(int[] arr, int k) {
         HashMap<Integer, Integer> fmap = new HashMap<>();
         // add element from 0 to k-1
         for(int i = 0; i < k - 1; i++) {
@@ -94,7 +94,7 @@ public class hmap {
 	}
 
     // Check If An Array Can Be Divided Into Pairs Whose Sum Is Divisible By K, portal
-    public static void solution(int[] arr, int k){
+    public static void solution2(int[] arr, int k){
 		HashMap<Integer, Integer> map = new HashMap<>();
         // prepare freq map of remainders
         for(int val : arr) {
@@ -497,7 +497,207 @@ public class hmap {
         return ansLen;
 	}
 
-    public static void main(String[] args) {
+    // count substrings with non repeating character
+    private static int countSubStringsNonRepeating(String str) {
+        int count = 0;
+        int acq = -1;
+        int rel = -1;
 
+        HashMap<Character, Integer> map = new HashMap<>();
+        while(true) {
+            boolean acflag = false;
+            boolean reflag = false;
+
+            while(acq < str.length() - 1) {
+                acflag = true;
+                acq++;
+                char ch = str.charAt(acq);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+                if(map.get(ch) == 2) {
+                    // int n = acq - rel - 1;
+                    // count += n * (n + 1) / 2;
+                    break;
+                } else {
+                    count += acq - rel;
+                }
+            }
+
+
+            while(rel < acq) {
+                reflag = true;
+                rel++;
+                char ch = str.charAt(rel);
+
+                map.put(ch, map.get(ch) - 1);
+                if(map.get(ch) == 1) {
+                    count += acq - rel;
+                    break;
+                }
+            }
+
+            if(acflag == false && reflag == false) break;
+        }
+        return count;
     }
+
+
+    private static int longestKUniqueCharacter(String str, int k) {
+        int ans = -1;
+
+        int acq = -1;
+        int rel = -1;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while(true) {
+            boolean acflag = false;
+            boolean reflag = false;
+
+            while(acq < str.length() - 1) {
+                acflag = true;
+                acq++;
+                char ch = str.charAt(acq);
+
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+                if(map.size() < k) {
+                    continue;
+                } else if(map.size() == k) {
+                    ans = Math.max(ans, acq - rel);
+                } else {
+                    break;
+                }
+            }
+
+            while(rel < acq) {
+                reflag = true;
+                rel++;
+                char ch = str.charAt(rel);
+                map.put(ch, map.get(ch) - 1);
+
+                if(map.get(ch) == 0) {
+                    map.remove(ch);
+                }
+
+                if(map.size() > k) {
+                    continue;
+                } else if(map.size() == k) {
+                    break;
+                }
+            }
+
+            if(acflag == false && reflag == false) break;
+        }
+        return ans;
+    }
+
+    private static int handleWhenK1(String str) {
+        int acq = -1;
+        int rel = -1;
+        int count = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while(true) {
+            boolean flag1 = false;
+            boolean flag2 = false;
+
+            while(acq < str.length() - 1) {
+                flag1 = true;
+                acq++;
+                char ch = str.charAt(acq);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+                if(map.size() == 2) {
+                    map.remove(ch);
+                    acq--;
+                    break;
+                }
+            }
+
+            while(rel < acq) {
+                count += acq - rel;
+                rel++;
+                char ch = str.charAt(rel);
+                map.put(ch, map.get(ch) - 1);
+                if(map.get(ch) == 0) map.remove(ch);
+
+                if(map.size() == 0) break;
+            }
+
+            if(flag1 == false && flag2 == false) {
+                break;
+            }
+        }
+
+        return count;
+    }
+
+
+    private static int countSubStringExactlyK(String str, int k) {
+        if(k == 1) {
+            return handleWhenK1(str);
+        }
+        HashMap<Character, Integer> mapb = new HashMap<>();
+
+        int count = 0;
+
+        int is = -1;
+        int ib = -1;
+        int j = -1;
+
+        while(true) {
+            boolean flag1 = false;
+            boolean flag2 = false;
+            boolean flag3 = false;
+            // k - 1 for map small
+            while(is < str.length() - 1) {
+                flag1 = true;
+                is++;
+                char ch = str.charAt(is);
+                maps.put(ch, maps.getOrDefault(ch, 0) + 1);
+                if(maps.size() == k) {
+                    maps.remove(ch);
+                    is--;
+                    break;
+                }
+            }
+            // k for map big
+            while(ib < str.length() - 1) {
+                flag2 = true;
+                ib++;
+                char ch = str.charAt(ib);
+                mapb.put(ch, mapb.getOrDefault(ch, 0) + 1);
+                if(mapb.size() == k + 1) {
+                    mapb.remove(ch);
+                    ib--;
+                    break;
+                }
+            }
+            // release
+            while(j < is) {
+                flag3 = true;
+                if(mapb.size() == k && maps.size() == k - 1) {
+                    count += ib - is;
+                }
+                j++;
+                char ch = str.charAt(j);
+                // remove from maps
+                maps.put(ch, maps.get(ch) - 1);
+                if(maps.get(ch) == 0) maps.remove(ch);
+                // remove from mapb
+                mapb.put(ch, mapb.get(ch) - 1);
+                if(mapb.get(ch) == 0) mapb.remove(ch);
+
+                if(maps.size() < k - 1|| mapb.size() < k) {
+                    break;
+                }
+            }
+            if(flag1 == false && flag2 == false  && flag3 == false) break;
+        }
+        return count;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        
+    }
+
 }
