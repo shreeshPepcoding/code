32e854b39f9de7cbd544f4602a1b7ad5c4c60ceb
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2022-01-28 00:17:16 +0530

    update

--- pp/TSP/27_bitManipulation/bits.java
+++ pp/TSP/27_bitManipulation/bits.java
@@ -271,6 +271,68 @@ public class bits {
         }
     }
 
+    // all repeating thrice except one
+    public static void repeatThriceExceptOne1(int[] arr){
+        int res = 0;
+        for(int i = 0; i < 32; i++) {
+            int count = 0;
+            int bm = (1 << i);
+            for(int val : arr) {
+                // check if ith bit is ON or OFF in val
+                if((val & bm) == bm) {
+                    count++;
+                }
+            }
+            if(count % 3 == 1) {
+                // ON ith bit in result
+                res = (res | bm);
+            }
+        }   
+        System.out.println(res);
+    }
+
+    public static void repeatThriceExceptOne2(int[] arr){
+        int n3p0 = (~0); // all bits are 1
+        int n3p1 = 0; // all bits are 0
+        int n3p2 = 0; // all bits are 0
+
+        for(int val : arr) {
+            int cn3p1 = (n3p0 & val);
+            int cn3p2 = (n3p1 & val);
+            int cn3p3 = (n3p2 & val);
+
+            // off in same bit and on in next one
+            // cn3p1 -> OFF in n3p0 and ON in n3p1
+            n3p0 = (n3p0 & (~cn3p1));
+            n3p1 = (n3p1 | cn3p1);
+            
+            // cn3p2 -> OFF in n3p1 and ON in n3p2
+            n3p1 = (n3p1 & (~cn3p2));
+            n3p2 = (n3p2 | cn3p2);
+            
+            // cn3p3 -> OFF in n3p2 and ON in n3p0
+            n3p2 = (n3p2 & (~cn3p3));
+            n3p0 = (n3p0 | cn3p3);
+        }
+        System.out.println(n3p1);
+    }
+
+    // triplet 1
+    public static void triplet1(int[] arr){
+        int count = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            int xor = arr[i];
+            for(int k = i + 1; k < arr.length; k++) {
+                xor = (xor ^ arr[k]);
+                if(xor == 0) {
+                    count += (k - i);
+                } 
+            }
+        }
+        System.out.println(count);
+    }
+
 
     public static void main(String[] args) {
         System.out.println();
