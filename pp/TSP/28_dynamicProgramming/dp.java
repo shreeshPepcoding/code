c8944553df86fd8a1b2a01143a7d531428dfe70a
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2022-02-07 23:22:06 +0530

    update

--- pp/TSP/28_dynamicProgramming/dp.java
+++ pp/TSP/28_dynamicProgramming/dp.java
@@ -166,6 +166,64 @@ public class dp {
         return ans;
     }
 
+    // lbs -> longest bitonic subsequence
+    private static int lbs(int[] arr) {
+        int[] lis_ = lis(arr); // make a function of LIS and LDS which return DP rather than answer
+        int[] lds_ = lds(arr);
+
+        int ans = 0;
+        for(int i = 0; i < arr.length; i++) {
+            Math.max(ans, lis_[i] + lds_[i] - 1);
+        }
+        return ans;
+    }
+
+    // maximm non overlapping bridges
+    private static class Bridges implements Comparable<Bridges>{
+        int n;
+        int s;
+
+        public Bridges(int n, int s) {
+            this.n = n;
+            this.s = s;
+        }
+
+        public int compareTo(Bridges o) {
+            if(this.n != o.n) {
+                return this.n - o.n;
+            } else {
+                return this.s - o.s;
+            }
+        }
+    }
+
+    private static int nonOverlappingBridges(int[][] bridges) {
+        Bridges[] arr = new Bridges[bridges.length];
+        for(int i = 0; i < bridges.length; i++) {
+            arr[i] = new Bridges(bridges[i][0], bridges[i][1]);
+        }
+
+        // ort north pole
+        Arrays.sort(arr);
+        // LIS on south pole
+        int[] dp = new int[arr.length];
+        int ans = 1;
+        dp[0] = 1;
+        for(int i = 1; i < arr.length; i++) {
+            int max = 0;
+            for(int j = i - 1; j >= 0; j--) {
+                if(arr[j].s < arr[i].s) {
+                    max = Math.max(max, dp[j]);
+                }
+            }
+            dp[i] = max + 1;
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
+    }
+
+    // russian doll envelope
+    // code is exactle same as above one, i.e. Non overlapping bridges
 
     public static void main(String[] args) {
 
