727f970937f046d41bd1a626b0d1b8fe715ed810
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2022-03-13 19:13:41 +0530

    update

--- pp/TSP/29_LinkedListL2Revision/doublyLinkedList.java
+++ pp/TSP/29_LinkedListL2Revision/doublyLinkedList.java
@@ -1,3 +1,195 @@
 public class doublyLinkedList {
     
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode prev;
+
+        ListNode() {
+            this.val = 0;
+            this.next = this.prev = null;
+        }
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = this.prev = null;
+        }
+
+        ListNode(int val, ListNode next, ListNode prev) {
+            this.val = val;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    public ListNode head = null;
+    public ListNode tail = null;
+    public int size = 0;
+
+    public int size() {
+        return this.size;
+    }
+
+    public void display() {
+        System.out.print("NULL");
+        ListNode temp = this.head;
+        while(temp != null) {
+            System.out.print(" <--> " + temp.val);
+            temp = temp.next;
+        }
+        System.out.print(" <--> NULL");
+    }
+
+    private void handleAddWhenSize0(int val) {
+        ListNode nn = new ListNode(val);
+        this.head = this.tail = nn;
+        this.size = 1;
+    }
+
+    public void addFirst(int val) {
+        if(this.size == 0) {
+            this.handleAddWhenSize0(val);
+        } else {
+            ListNode nn = new ListNode(val);
+            nn.next = this.head;
+            nn.prev = null;
+            this.head.prev = nn;
+            this.head = nn;
+            this.size++;
+        }
+    }
+
+    public void addLast(int val) {
+        if(this.size == 0) {
+            this.handleAddWhenSize0(val);
+        } else {
+            ListNode nn = new ListNode(val);
+            nn.prev = this.tail;
+            nn.next = null;
+            this.tail.next = nn;
+            this.tail = nn;
+            this.size++;
+        }
+    }
+
+    private ListNode getNthNode(int indx) {
+        ListNode temp = this.head;
+        for(int i = 0; i < indx; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public void addAt(int val, int pos) {
+        if(pos < 0 || pos > this.size) {
+            System.out.println("Invalid Index");
+        } else if(pos == 0) {
+            this.addFirst(val);
+        } else if(pos == this.size) {
+            this.addLast(val);
+        } else {
+            ListNode n1 = this.getNthNode(pos - 1); // (n-1)th node
+            ListNode n2 = n1.next;
+
+            ListNode nn = new ListNode(val);
+            n1.next = nn;
+            nn.prev = n1;
+            nn.next = n2;
+            n2.prev = nn;
+            this.size++;
+        }
+    }
+
+    public int getFirst() {
+        if(this.size == 0) {
+            return -1;
+        } else {
+            return this.head.val;
+        }
+    }
+
+    public int getLast() {
+        if(this.size == 0) {
+            return -1;
+        } else {
+            return this.tail.val;
+        }
+    }
+
+    public int getAt(int pos) {
+        if(pos < 0 || pos >= this.size) {
+            return -1;
+        } else if(pos == 0) {
+            return this.getFirst();
+        } else if(pos == this.size - 1) {
+            return this.getLast();
+        } else {
+            ListNode nn = this.getNthNode(pos);
+            return nn.val;
+        }
+    }
+
+    private int handleRemoveWhenSize1() {
+        int data = this.head.val;
+        this.head = this.tail = null;
+        this.size = 0;
+        return data;
+    }
+
+    public int removeFirst() {
+        if(this.size == 0) {
+            return -1;
+        } else if(this.size == 1) {
+            return this.handleRemoveWhenSize1();
+        } else {
+            int data = this.head.val;
+            this.head = this.head.next;
+            this.head.prev = null;
+            this.size--;
+            return data;
+        }
+    }
+
+    public int removeLast() {
+        if(this.size == 0) {
+            return -1;
+        } else if(this.size == 1) {
+            return this.handleRemoveWhenSize1();
+        } else {
+            int data = this.tail.val;
+            this.tail = this.tail.prev;
+            this.tail.next = null;
+            this.size--;
+            return data;
+        }
+    }
+
+    public int removeAt(int pos) {
+        if(this.size == 0 || pos < 0 || pos >= this.size) {
+            return -1;
+        } else if(pos == 0) {
+            return this.removeFirst();
+        } else if(pos == this.size - 1) {
+            return this.removeLast();
+        } else {
+            ListNode n1 = getNthNode(pos - 1);
+            ListNode n = n1.next;
+            ListNode n2 = n.next;
+
+            n1.next = n2;
+            n2.prev = n1;
+            int data = n.val;
+            n.next = n.prev = null;
+            this.size--;
+            return data;
+        }
+    }
+
+ 
+    public static void fun() {
+
+    }
+    public static void main(String[] args) {
+        fun();    
+    }
 }
--- pp/TSP/29_LinkedListL2Revision/lruCache.java
+++ pp/TSP/29_LinkedListL2Revision/lruCache.java
@@ -1,3 +1,107 @@
+import java.util.HashMap;
+
 public class lruCache {
+    class LRUCache {
+
+        class Node {
+            int key;
+            int value;
+            Node next;
+            Node prev;
+
+            Node(int key, int value) {
+                this.key = key;
+                this.value = value;
+                this.next = this.prev = null;
+            }
+        }
+
+        private HashMap<Integer, Node> map = null;
+        private Node head = null;
+        private Node tail = null;
+        private int size = 0;
+        private int cap = 0;
+
+        private void addLast(Node node) {
+            if(this.size == 0) {
+                this.head = this.tail = node;
+                this.size = 1;
+            } else {
+                this.tail.next =node;
+                node.prev = this.tail;
+                this.tail = node;
+                this.size++;
+            }
+        }
+
+        private void removeNode(Node node) {
+            if(this.size == 1) {
+                this.head = this.tail = null;
+            } else if(node == this.head) {
+                this.head = this.head.next;
+                this.head.prev = null;
+            } else if(node == this.tail) {
+                this.tail = this.tail.prev;
+                this.tail.next = null;
+            } else {
+                Node n1 = node.prev;
+                Node n2 = node.next;
+                n1.next = n2;
+                n2.prev = n1;
+                node.next = node.prev = null;
+            }
+            this.size--;
+        }
     
+        private int removeFirst() {
+            // there is always more than one element
+            Node rem = head;
+            head = head.next;
+            head.prev = null;
+            rem.next = null;
+            this.size--;
+            return rem.key;
+        }
+
+        public LRUCache(int capacity) {
+            this.cap = capacity;
+            this.map = new HashMap<>();
+        }
+        
+        public int get(int key) {
+            if(map.containsKey(key)) {
+                // get value 
+                int value = map.get(key).value;
+                // set it in top
+                this.put(key, value);
+                return value;
+            } else {
+                return -1;
+            }
+        }
+        
+        public void put(int key, int value) {
+            if(map.containsKey(key)) {
+                // updation
+                Node node = map.get(key);
+                node.value = value;
+                this.removeNode(node);
+                this.addLast(node);
+            } else {
+                // addition of next key-value
+                Node node = new Node(key, value);
+                map.put(key, node);
+                this.addLast(node);
+
+                if(this.size > cap) {
+                    int removalKey = this.removeFirst();
+                    map.remove(removalKey);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedHashMap
+    }
 }
