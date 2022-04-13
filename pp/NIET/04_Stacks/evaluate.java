d47c780397356d6d27679e122841ee74b168f465
shreeshPepcoding <77565726+shreeshPepcoding@users.noreply.github.com>
2022-04-13 13:44:51 +0530

    update

--- pp/NIET/04_Stacks/evaluate.java
+++ pp/NIET/04_Stacks/evaluate.java
@@ -236,15 +236,62 @@ public class evaluate {
 
     // prefix
     public static void prefixEvaluation(String str) {
+        Stack<Integer> vstack = new Stack<>();
 
+        for(int i = str.length() - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+
+            if(ch >= '0' && ch <= '9') {
+                vstack.push((int)(ch - '0'));
+            } else {
+                int val1 = vstack.pop();
+                int val2 = vstack.pop();
+
+                int res = evaluate(val1, val2, ch);
+                vstack.push(res);
+            }
+        }
+        System.out.println(vstack.pop());
     }
 
     public static void prefixToPostfix(String str) {
+        Stack<String> vstack = new Stack<>();
 
+        for(int i = str.length() - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+
+            if(ch >= '0' && ch <= '9') {
+                vstack.push("" + ch);
+            } else {
+                String val1 = vstack.pop();
+                String val2 = vstack.pop();
+
+                // int res = evaluate(val1, val2, ch);
+                String res = val1 + val2 + ch;
+                vstack.push(res);
+            }
+        }
+        System.out.println(vstack.pop());
     }
 
     public static void prefixToInfix(String str) {
+        Stack<String> vstack = new Stack<>();
 
+        for(int i = str.length() - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+
+            if(ch >= '0' && ch <= '9') {
+                vstack.push("" + ch);
+            } else {
+                String val1 = vstack.pop();
+                String val2 = vstack.pop();
+
+                // int res = evaluate(val1, val2, ch);
+                String res = "(" + val1 + ch +  val2 + ")";
+                vstack.push(res);
+            }
+        }
+        System.out.println(vstack.pop());
     }
 
     public static void fun() {
